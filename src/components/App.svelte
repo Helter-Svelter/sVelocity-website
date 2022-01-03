e3be895cc78e6c629d4009261667e8c4039d1f1c
AllieMarzieh <alliegramy1993@gmail.com>
2022-01-03 15:58:03 -0500

    Added a nav bar

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,12 @@
-<script> 
-import Navbar from "./Navbar.svelte"
- </script>  
- <Navbar/>
-<h1> hello world! </h1> 
+<script>
+  import Navbar from './Navbar.svelte';
+</script>
+<Navbar />
+<h1>hello world!</h1>
+
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
