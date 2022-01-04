dc3f6fe3e908bd3fd1b9ee0dbdd5a27223c2c10c
tehyaarassman <tehyaarassman@gmail.com>
2022-01-04 17:14:14 -0600

    added more avatars

--- src/components/MeetTeam.svelte
+++ src/components/MeetTeam.svelte
@@ -1,28 +1,71 @@
-
-<script> 
-import Tehya from '../assets/Tehya.png'
-
+<script>
+  import Tehya from '../assets/Tehya.png';
+  import Eleni from '../assets/Eleni.png';
+  import Allie from '../assets/Allie.png';
 </script>
-<h3 class="team"> sVelocity Team </h3>
-<div class="team-outer-container">
-<div class="avatar1">
-      <img class="avatar" src={Tehya} alt="Avatar 1">
-      <a id="github-icon" href="https://github.com/tehyaarassman"
-      ><i class="fa fa-github" style="font-size:36px; color: #120563;" /></a
 
-    > 
-    <a class="linkedin-img" href="https://www.linkedin.com/in/tehya-rassman/">
-        <i class="fa fa-linkedin-square" style="font-size:36px; color: #120563;" />
+<h3 class="team">sVelocity Team</h3>
+<div class="team-outer-container">
+  <div class="avatar1">
+    <img class="avatar" src={Tehya} alt="Avatar 1" />
+    <div class="name"><p>Tehya Rassman</p></div>
+    <div class="icon-div">
+      <a id="github-icon" href="https://github.com/tehyaarassman"
+        ><i class="fa fa-github" style="font-size:36px; color: #120563;" /></a
+      >
+      <a class="linkedin-img" href="https://www.linkedin.com/in/tehya-rassman/">
+        <i
+          class="fa fa-linkedin-square"
+          style="font-size:36px; color: #120563;"
+        />
       </a>
-      <!-- add a tag for my name, and the github and linkded in -->
+    </div>
+    <!-- add a tag for my name, and the github and linkded in -->
+  </div>
+
+  <div class="avatar2">
+    <img class="avatar" src={Eleni} alt="Avatar 2" />
+    <div class="name"><p>Eleni Zoumpoulaki</p></div>
+    <div class="icon-div">
+      <a id="github-icon" href="https://github.com/elenizoump"
+        ><i class="fa fa-github" style="font-size:36px; color: #120563;" /></a
+      >
+      <a class="linkedin-img" href="https://www.linkedin.com/in/elenizoump">
+        <i
+          class="fa fa-linkedin-square"
+          style="font-size:36px; color: #120563;"
+        />
+      </a>
+    </div>
+    <!-- add a tag for my name, and the github and linkded in -->
+  </div>
+
+  <div class="avatar3">
+    <img class="avatar" src={Allie} alt="Avatar 3" />
+    <div class="name"><p>Allison Gramy</p></div>
+    <div class="icon-div">
+      <a id="github-icon" href="https://github.com/AllieMarzieh"
+        ><i class="fa fa-github" style="font-size:36px; color: #120563;" /></a
+      >
+      <a class="linkedin-img" href="www.linkedin.com/in/Allisongramy">
+        <i
+          class="fa fa-linkedin-square"
+          style="font-size:36px; color: #120563;"
+        />
+      </a>
+    </div>
+    <!-- add a tag for my name, and the github and linkded in -->
+  </div>
 </div>
-</div>
+
+
+
 <style>
-    .team {
-    background-color: #eae8f8;
+  .team {
+    /* background-color: #eae8f8; */
     text-align: center;
   }
-    h3 {
+  h3 {
     margin: 0;
     padding: 20px 0 0 0;
     font-weight: 300;
@@ -30,16 +73,25 @@ import Tehya from '../assets/Tehya.png'
     margin-bottom: 15px;
     color: #0f0459;
   }
-  
-  .team-outer-container{
-      display: flex;
-      flex-wrap: wrap;
-      flex-direction: row;
-      margin-top: -48px;
+
+  .team-outer-container {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    /* margin-top: -48px; */
     width: calc(100% + 48px);
-    margin-left: -48px;
+    margin-bottom: 1%;
+    /* margin-left: -48px; */
   }
-  
+
+  .name{
+    text-align: center;
+  }
+
+  .icon-div {
+    text-align: center;
+  }
+
   .avatar1 {
     flex-basis: 25%;
     -webkit-box-flex: 0;
